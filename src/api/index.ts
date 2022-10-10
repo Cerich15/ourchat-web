4a215843ddcc514494baa954f42075d8d27a01f7
Marvin Petate <petatemarvin26@gmail.com>
2022-10-10 19:50:49 +0800

    setup/api
    setup api and rename from docks to reducks

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,17 @@
 import server from "./server";
 
-const apiConfig = (store: object) => {};
+import {request, response} from "./interceptor";
+
+const apiConfig = (store: object) => {
+  server.interceptors.request.use(
+    request.resolve(store),
+    request.reject(store),
+  );
+  server.interceptors.response.use(
+    response.resolve(store),
+    response.reject(store),
+  );
+};
 
 export {server};
 export default apiConfig;
